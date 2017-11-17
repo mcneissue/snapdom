b4d46eddc222a10105871eb242e0156ace85a1fe
John C. Lavelle <jlavelle604@gmail.com>
2017-11-16 19:49:16 -0500

    Fix spelling mistake

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import synchronousStateManager from './state_managers/synchronous'
-import withThunkHander from './state_managers/with_thunk'
+import withThunkHandler from './state_managers/with_thunk'
 import snap from './snap'
 
 export default {
   synchronousStateManager,
-  withThunkHander,
+  withThunkHandler,
   snap
 }
